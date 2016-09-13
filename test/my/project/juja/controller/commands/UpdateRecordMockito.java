c69069eae2932603df604f573978f7c648d7520a
Nikolay <Horushko.Nikolay@gmail.com>
2016-09-13 21:05:10 +0300

    refactor DataBase Tests delete non actual tests

--- test/my/project/juja/controller/commands/UpdateRecordMockito.java
+++ test/my/project/juja/controller/commands/UpdateRecordMockito.java
@@ -170,8 +170,4 @@ public class UpdateRecordMockito {
         assert (flag);
     }
 
-
-
-
-
 }
--- test/my/project/juja/model/DataBaseTest.java
+++ test/my/project/juja/model/DataBaseTest.java
@@ -1,34 +1,52 @@
-//package my.project.juja.model;
-//
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
-//import org.junit.rules.ExpectedException;
-//
-//import java.util.List;
-//import java.util.Set;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertTrue;
-//
-///**
-// * Created by Nikol on 4/15/2016.
-// */
-//public class DataBaseTest {
-//    DataBase DataBase;
-//    @Before
-//    public void setup() {
-//        DataBase = new DataBase();
-//        DataBase.getConnection("sqlcmd", "postgres", "root");
-//    }
-//
-//
-//    @Rule
-//    public ExpectedException expectedEx = ExpectedException.none();
-//
-//    @Test
-//    public void testAddRecord(){
-//
+package my.project.juja.model;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Nikol on 4/15/2016.
+ */
+public class DataBaseTest {
+    DataBase DataBase;
+    @Before
+    public void setup() {
+        DataBase = new DataBase();
+        DataBase.getConnection("sqlcmd", "postgres", "root");
+    }
+
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+
+    @Test
+    public void testAddRecordWrongTableName(){
+        expectedEx.expect(RuntimeException.class);
+        DataBase.clearTable("wrongTableName");
+    }
+
+    @Test
+    public void getTableListTest(){
+        Set<String> actual = DataBase.getTableList();
+
+        assertEquals("[users, users2]", actual.toString());
+    }
+
+    @Test
+    public void getTableDataWithWrongTableName(){
+        expectedEx.expect(RuntimeException.class);
+        DataBase.getTableData("wrongTableName");
+    }
+
+    @Test
+    public void testAddRecord(){
+
 //        DataBase.clearTable("users");
 //
 //        DataBase.addRecord("users","name password Id", "alex FadH74Gne 123");
@@ -37,35 +55,11 @@
 //        String actual = recordList.get(0);
 //
 //        assertEquals("alex|FadH74Gne|123|", actual);
-//
-//    }
-//
-//    @Test
-//    public void testAddRecordWrongTableName(){
-//        expectedEx.expect(RuntimeException.class);
-//        DataBase.clearTable("wrongTableName");
-//    }
-//
-//    @Test
-//    public void getColumnNameTest(){
-//        List<String> actual = DataBase.getColumnName("users");
-//        assertEquals("[name, password, Id]", actual.toString());
-//    }
-//    @Test
-//    public void getColumnNameWithWrongTableName() {
-//        expectedEx.expect(RuntimeException.class);
-//        DataBase.getColumnName("wrongTableName");
-//    }
-//
-//    @Test
-//    public void getTableListTest(){
-//        Set<String> actual = DataBase.getTableList();
-//
-//        assertEquals("[users, users2]", actual.toString());
-//    }
-//
-//    @Test
-//    public void getTableData(){
+
+    }
+
+    @Test
+    public void getTableData(){
 //        DataBase.clearTable("users");
 //        DataBase.addRecord("users","name password Id", "login1 pass1 1");
 //        DataBase.addRecord("users","name password Id", "login2 pass2 2");
@@ -80,19 +74,14 @@
 //        assertEquals(   "login1|pass1|1|\n" +
 //                        "login2|pass2|2|\n" +
 //                        "login3|pass3|3|\n", actual);
-//    }
-//
-//    @Test
-//    public void getTableDataWithWrongTableName(){
-//        expectedEx.expect(RuntimeException.class);
-//        DataBase.getTableData("wrongTableName");
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//}
+    }
+
+
+
+
+
+
+
+
+
+}
