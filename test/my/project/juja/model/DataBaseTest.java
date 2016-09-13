7d9e10ba8a20c68922d8aa92b073e0fd94c05498
Nikolay <Horushko.Nikolay@gmail.com>
2016-09-13 21:28:42 +0300

    added test for class DataBase for methods addRecord and getTableData

--- test/my/project/juja/model/DataBaseTest.java
+++ test/my/project/juja/model/DataBaseTest.java
@@ -1,10 +1,18 @@
 package my.project.juja.model;
 
+import my.project.juja.controller.commands.table.Cell;
+import my.project.juja.controller.commands.table.CellInfo;
+import my.project.juja.controller.commands.table.Row;
+import my.project.juja.controller.commands.table.Table;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import javax.swing.text.TabableView;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
@@ -13,11 +21,12 @@ import static org.junit.Assert.assertEquals;
  * Created by Nikol on 4/15/2016.
  */
 public class DataBaseTest {
-    DataBase DataBase;
+    private DataBase dataBase;
+    private static final String tableName = "users";
     @Before
     public void setup() {
-        DataBase = new DataBase();
-        DataBase.getConnection("sqlcmd", "postgres", "root");
+        dataBase = new DataBase();
+        dataBase.getConnection("sqlcmd", "postgres", "root");
     }
 
 
@@ -28,12 +37,12 @@ public class DataBaseTest {
     @Test
     public void testAddRecordWrongTableName(){
         expectedEx.expect(RuntimeException.class);
-        DataBase.clearTable("wrongTableName");
+        dataBase.clearTable("wrongTableName");
     }
 
     @Test
     public void getTableListTest(){
-        Set<String> actual = DataBase.getTableList();
+        Set<String> actual = dataBase.getTableList();
 
         assertEquals("[users, users2]", actual.toString());
     }
@@ -41,31 +50,31 @@ public class DataBaseTest {
     @Test
     public void getTableDataWithWrongTableName(){
         expectedEx.expect(RuntimeException.class);
-        DataBase.getTableData("wrongTableName");
+        dataBase.getTableData("wrongTableName");
     }
 
     @Test
-    public void testAddRecord(){
-
-//        DataBase.clearTable("users");
-//
-//        DataBase.addRecord("users","name password Id", "alex FadH74Gne 123");
-//
-//        List<String> recordList = DataBase.getTableData("users");
-//        String actual = recordList.get(0);
-//
-//        assertEquals("alex|FadH74Gne|123|", actual);
-
+    public void testAddRecord() throws SQLException {
+        dataBase.clearTable(tableName);
+        Table expectedTable = new Table(tableName, dataBase.getColumnInformation(tableName));
+        Row row = new Row(expectedTable.getCellInfos());
+        for (Cell cell : row.getCells()) {
+            cell.setValue("235", true);
+        }
+        expectedTable.addRow(row);
+        dataBase.addRecord(expectedTable);
+        Table actualTable = dataBase.getTableData(tableName);
+        assertEquals(actualTable.toString(), expectedTable.toString());
     }
 
     @Test
     public void getTableData(){
-//        DataBase.clearTable("users");
-//        DataBase.addRecord("users","name password Id", "login1 pass1 1");
-//        DataBase.addRecord("users","name password Id", "login2 pass2 2");
-//        DataBase.addRecord("users","name password Id", "login3 pass3 3");
+//        dataBase.clearTable("users");
+//        dataBase.addRecord("users","name password Id", "login1 pass1 1");
+//        dataBase.addRecord("users","name password Id", "login2 pass2 2");
+//        dataBase.addRecord("users","name password Id", "login3 pass3 3");
 //
-//        List<String> actualList = DataBase.getTableData("users");
+//        List<String> actualList = dataBase.getTableData("users");
 //        String actual = "";
 //        for (String s : actualList) {
 //            actual += s + "\n";
