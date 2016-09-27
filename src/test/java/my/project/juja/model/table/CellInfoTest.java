2519615fd9c0c29ecb3bac80621cf3caf5907d70
Nikolay <Horushko.Nikolay@gmail.com>
2016-09-27 20:19:56 +0300

    add tests for CellInfo

--- src/test/java/my/project/juja/model/table/CellInfoTest.java
+++ src/test/java/my/project/juja/model/table/CellInfoTest.java
@@ -1,8 +1,35 @@
 package my.project.juja.model.table;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Nikol on 9/27/2016.
  */
 public class CellInfoTest {
-    
+    CellInfo testCellInfo;
+
+    @Before
+    public void setup() {
+        testCellInfo = new CellInfo("TestColumnName", "integer", false, false, 0);
+    }
+
+    @Test
+    public void getTypeTest() {
+        assertEquals("integer", testCellInfo.getType());
+    }
+
+    @Test
+    public void getLengthTest() {
+        assertEquals(0, testCellInfo.getLength());
+    }
+
+    @Test
+    public void isHasDefault() {
+        assertEquals(false, testCellInfo.isHasDefault());
+    }
+
 }
